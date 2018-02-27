b7365d565b8e52cd377bc49849afeb551de3c9c5
Emil Wismann <kirkebak@danfoss.com>
2018-02-27 14:45:36 +0100

    Added gui project

---
+++ CarWash.gui/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarWash.gui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CarWash v1.0");
+            Console.WriteLine("1. Wash Hall");
+            Console.WriteLine("2. Statistics");
+            switch (Console.ReadLine()) {
+                case "1": {
+                        break;
+                    }
+                case "2":
+                    {
+                        break;
+                    }
+            }
+        }
+    }
+}
